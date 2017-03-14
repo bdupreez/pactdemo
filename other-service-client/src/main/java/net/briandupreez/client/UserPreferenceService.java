56a8d08263d89a1ebc05df27ca398cee3d80d880
brian <bdupreez@gmail.com>
2017-03-14 07:04:44 +0200

    GetMapping annotation

--- other-service-client/src/main/java/net/briandupreez/client/UserPreferenceService.java
+++ other-service-client/src/main/java/net/briandupreez/client/UserPreferenceService.java
@@ -1,25 +1,22 @@
 package net.briandupreez.client;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import net.briandupreez.provider.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 
-import net.briandupreez.provider.User;
-
 @RestController
 public class UserPreferenceService {
 
 
-    @RequestMapping(method = RequestMethod.GET, value = "/user/preference")
+    @GetMapping("/user/preference")
     public UserPreferences retrieveUserPreference() {
         final UserPreferences userPreferences = new UserPreferences();
 
         RestTemplate restTemplate = new RestTemplate();
         final User user = restTemplate.getForObject("http://localhost:8080/user", User.class);
-
 
         userPreferences.setUserName(user.getUserName());
         userPreferences.setUserId("2");
--- service-provider/src/main/java/net/briandupreez/provider/UserService.java
+++ service-provider/src/main/java/net/briandupreez/provider/UserService.java
@@ -2,27 +2,25 @@ package net.briandupreez.provider;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class UserService {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/user/{userId}", produces = "application/json")
+    @GetMapping(value = "/user/{userId}", produces = "application/json")
     public ResponseEntity<User> retrieveUser(@PathVariable String userId) {
         final User user = new User();
-        if(userId.equals("3")){
+        if (userId.equals("3")) {
             user.setUserName("John");
             user.setFirstName("John");
             user.setUserId("3");
-        }else {
+        } else {
             user.setUserName("Bob");
             user.setUserId("1");
         }
-        System.out.println("Called...");
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
